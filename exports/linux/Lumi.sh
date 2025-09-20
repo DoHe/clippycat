091721de2891ab0dbb4928b1dbd73e1062825b5b
Dominik Henter <wahlholz@gmx.de>
2025-09-20 11:34:09 +0200

    Use git lfs

--- exports/linux/Lumi.sh
+++ exports/linux/Lumi.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
-printf '\033c\033]0;%s\a' Lumi
-base_path="$(dirname "$(realpath "$0")")"
-"$base_path/Lumi.x86_64" "$@"
+version https://git-lfs.github.com/spec/v1
+oid sha256:71b52fd45043e07e0d0f742ec8d7c25d4489fc18b4583c7a7bd62d5b460ad7c8
+size 113
